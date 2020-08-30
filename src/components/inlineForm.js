269be60e614e247c35bdcf515513e3a126e20baf
yogin16 <yogin.daiict@gmail.com>
2020-08-30 21:38:36 +0530

    add input ref to the form + add dynamic form builder

--- src/components/inlineForm.js
+++ src/components/inlineForm.js
@@ -2,12 +2,12 @@ import React from "react"
 
 import { Form, Button } from "react-bootstrap"
 
-const InlineForm = ({ label, placeholder, onClick }) => {
+const InlineForm = ({ label, placeholder, inputRef, onClick }) => {
     return (
         <Form inline>
             <Form.Group controlId="numberOfYears">
                 <Form.Label>{label}</Form.Label>
-                <Form.Control type="number" className="mx-sm-3" placeholder={placeholder} />
+                <Form.Control ref={inputRef} type="number" className="mx-sm-3" placeholder={placeholder} />
             </Form.Group>
             <Button variant="primary" type="submit" onClick={onClick}>
                 Submit
--- src/pages/rate.js
+++ src/pages/rate.js
@@ -1,12 +1,75 @@
 import React from "react"
-import { useState } from "react"
-import { Row, Col, Container } from "react-bootstrap"
+import { useState, useRef, createRef } from "react"
+import { Row, Col, Container, Form } from "react-bootstrap"
 
 import Layout from "../components/layout"
 import InlineForm from "../components/inlineForm"
 
+
+const CFInput = ({ yearNo, yearRef }) => {
+    var controlId = "year" + yearNo
+    return (
+        <Col>
+            <Form inline>
+                <Form.Group controlId={controlId}>
+                    <Form.Label>{"Cash Flow for Year: " + yearNo}</Form.Label>
+                    <Form.Control ref={yearRef} type="number" className="mx-sm-3" />
+                </Form.Group>
+            </Form>
+        </Col>
+    )
+}
+
+
+const CFInputGroup = ({ numYears }) => {
+    const refs = {}
+    const rows = [];
+    numYears = parseInt(numYears);
+    for (var i = 1; i < numYears + 1; i++) {
+        const ref = createRef();
+        refs[i] = ref;
+        rows.push(
+            <CFInput yearNo={i} yearRef={ref} />
+        );
+    }
+    return (
+        <Container>
+            {
+                rows.map((val, idx) => {
+                    return (
+                        <Row key={idx}>
+                            {val}
+                        </Row>
+                    )
+                })
+            }
+        </Container>
+    )
+}
+
+
+const RateForm = ({ show, yearsRef }) => {
+    if (show) {
+        return (
+            <Col>
+                <p>Hello!</p>
+                <CFInputGroup numYears={yearsRef.current.value} />
+            </Col>
+        )
+    } else {
+        return (
+            <Col>
+                <p>Test!</p>
+            </Col>
+        )
+    }
+
+}
+
+
 function Rate() {
     const [show, setShow] = useState(false);
+    const inputYears = useRef(null);
 
     return (
         <Layout pageInfo={{ pageName: "rate" }}>
@@ -16,6 +79,7 @@ function Rate() {
                         <InlineForm
                             label="Number of Years"
                             placeholder="Enter # of years"
+                            inputRef={inputYears}
                             onClick={
                                 (e) => {
                                     e.preventDefault()
@@ -25,14 +89,9 @@ function Rate() {
                         />
                     </Col>
                 </Row>
-                {
-                    show && 
-                    <Row>
-                        <Col>
-                            <p>Hello!</p>
-                        </Col>
-                    </Row>
-                }
+                <Row>
+                    <RateForm show={show} yearsRef={inputYears} />
+                </Row>
             </Container>
         </Layout>
     )
